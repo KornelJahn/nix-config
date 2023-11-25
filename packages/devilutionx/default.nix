9b046f9f7c3934c828a631dd4ffa69a1178e799a
Kornel Jahn <kjahn.public@gmail.com>
2023-11-25 17:41:46 +0100

    devilutionx: Bump version.

--- packages/devilutionx/default.nix
+++ packages/devilutionx/default.nix
@@ -6,13 +6,13 @@ in
 if system == "x86_64-linux" then
   let
     pname = "devilutionx";
-    version = "1.5.0";
+    version = "1.5.1";
   in
   pkgs.appimageTools.wrapType2 {
     inherit pname version;
     src = pkgs.fetchurl {
       url = "https://github.com/diasurgical/devilutionX/releases/download/${version}/devilutionx-linux-x86_64.appimage";
-      sha256 = "sha256-mFHMXYnB5DUmbZfIb7MFDUoo4v4wTWiS+/Yo+QR2gQM=";
+      sha256 = "sha256-LKNftM35VWSes0KNwO9vBOZAzfIyK3ZdMFQOLixyX68=";
     };
   }
 else
