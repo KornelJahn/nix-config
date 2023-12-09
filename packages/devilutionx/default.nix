ca87034b2e93833dce4eefac45eba2fc6de06f2b
Kornel Jahn <kjahn.public@gmail.com>
2023-12-09 21:57:27 +0100

    packages/devilutionx: Fix name.

--- packages/devilutionx/default.nix
+++ packages/devilutionx/default.nix
@@ -5,11 +5,11 @@ let
 in
 if system == "x86_64-linux" then
   let
-    pname = "devilutionx";
+    name = "devilutionx";
     version = "1.5.1";
   in
   pkgs.appimageTools.wrapType2 {
-    inherit pname version;
+    inherit name version;
     src = pkgs.fetchurl {
       url = "https://github.com/diasurgical/devilutionX/releases/download/${version}/devilutionx-linux-x86_64.appimage";
       sha256 = "sha256-LKNftM35VWSes0KNwO9vBOZAzfIyK3ZdMFQOLixyX68=";
