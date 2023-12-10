5dbd34193bf32a8b778afe77d639b65e059fbeb5
Kornel Jahn <kjahn.public@gmail.com>
2023-12-10 06:31:24 +0100

    nix-nixpkgs.nix: Switch to Nix 2.19.

--- nixos/modules/core/nix-nixpkgs.nix
+++ nixos/modules/core/nix-nixpkgs.nix
@@ -7,7 +7,7 @@
       dates = "weekly";
       options = "--delete-older-than 30d";
     };
-    package = pkgs.nixVersions.nix_2_18;
+    package = pkgs.unstable.nixVersions.nix_2_19;
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
